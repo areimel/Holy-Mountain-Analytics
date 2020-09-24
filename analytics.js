8c2a6b696f29933716fd3ae6fe6773a461b4432d
Alec Reimel <alecreimel1@gmail.com>
2020-09-24 13:18:54 -0400

    9/24

--- analytics.js
+++ analytics.js
@@ -1,3 +1,23 @@
+/********************************************
+*********************************************
+
+Holy Mountain - Analytics Plugin
+
+*********************************************
+********************************************/
+
+
+
+
+
+
+
+/********************************************
+
+PRE-FLIGHT CHECK
+
+********************************************/
+
 if (typeof jQuery == 'undefined') {
 
     // jQuery IS NOT loaded, do stuff here.
@@ -30,6 +50,35 @@ if (typeof jQuery == 'undefined') {
 }
 
 
+
+/********************************************
+
+INITIALIZATION
+
+********************************************/
+$(document).ready(function() {
+
+	//console log
+		console.log("---Holy Mountain Analytics initialized.---");
+
+	//GTM detect - check for GTM script in <head>
+		var gtm_detect = 0;
+		$('script').each(function() {
+			if($(this).text("https://www.googletagmanager.com/gtm.js?id=")){
+				gtm_detect = 1;
+				return false;
+			}
+		});
+
+		if(gtm_detect == 1){
+			console.log("GTM script detected.");
+		} else {
+			console.log("No GTM script detected.  Double check GTM installation.");
+		}
+});
+
+
+
 /********************************************
 
 GA EVENTS - MAIN 
@@ -38,86 +87,58 @@ Example:
 
 data-event="GAEvent" data-category="Home" data-label="CTA" data-action="Click" data-value="undefined"
 
-- 	The above code should be pasted into the opening tag, as data-attributes,
-	of whatever element you want to put a click event on.
-	The below codes then grabs the data attributes you've set,
-	and pipes it through GTM and GA
+- 	The above code should be pasted into the opening tag, 
+	as data-attributes, of whatever element you want to put a click event on.
+	The below codes then grabs the data attributes you've set,	and pipes it through GTM and GA
 
 ********************************************/
 $(document).ready(function(){
 
-	
-/*
-	$("[data-event='GAEvent']").click(function() {
-		var evCat = $(this).attr('data-category') 	? $(this).attr('data-category') : '',
-			evAct = $(this).attr('data-action') 	? $(this).attr('data-action') : '',
-			evLab = $(this).attr('data-label') 		? $(this).attr('data-label') : '',
-			evVal = $(this).attr('data-value') 		? $(this).attr('data-value') : '';
+
+	$.fn.eventfire_ready = function(){
+
+		console.log("Event Firing primed.  Ready for takeoff.");
+
+		$("[data-event='GAEvent']").click(function() {
+			//Set vars
+			var evCat = $(this).attr('data-category') 	? $(this).attr('data-category') : '';
+			var evAct = $(this).attr('data-action') 		? $(this).attr('data-action') : '';
+			var evLab = $(this).attr('data-label') 		? $(this).attr('data-label') : '';
+			var evVal = $(this).attr('data-value') 		? $(this).attr('data-value') : '';
 
 			try {
-
+				//Fire event
 				window.dataLayer = window.dataLayer || [];
 				dataLayer.push({
-					'event': 'GAEvent',
-					'eventCategory': evCat,
-					'eventAction': evAct,
-					'eventLabel': evLab,
-					'eventValue': evVal,
+					'event': 			'GAEvent',
+					'eventCategory': 	evCat,
+					'eventAction': 		evAct,
+					'eventLabel': 		evLab,
+					'eventValue': 		evVal,
 				});
+
 				console.log("GA Event fired - Event Category: ["+evCat+"], Event Label: ["+evLab+"], Event Action: ["+evAct+"]");
 
 			} catch (e) {
 				console.log("GA Event Error");
 			}
-	});
-*/
-
-	
-
-	$.fn.eventfire_ready = function(){
-
-		console.log("Event Firing primed");
-
-		$("[data-event='GAEvent']").click(function() {
-				var evCat = $(this).attr('data-category') 	? $(this).attr('data-category') : '',
-					evAct = $(this).attr('data-action') 	? $(this).attr('data-action') : '',
-					evLab = $(this).attr('data-label') 		? $(this).attr('data-label') : '',
-					evVal = $(this).attr('data-value') 		? $(this).attr('data-value') : '';
-
-					try {
-
-						window.dataLayer = window.dataLayer || [];
-						dataLayer.push({
-							'event': 'GAEvent',
-							'eventCategory': evCat,
-							'eventAction': evAct,
-							'eventLabel': evLab,
-							'eventValue': evVal,
-						});
-						console.log("GA Event fired - Event Category: ["+evCat+"], Event Label: ["+evLab+"], Event Action: ["+evAct+"]");
-
-					} catch (e) {
-						console.log("GA Event Error");
-					}
-			});
-
+		});
 	};
-
 });
 
 
 /********************************************
 
-GA AUTO-TAGGER - PLUGIN VERSION
+GA AUTO-TAGGER - PROGRAMMATIC VERSION
 
 ********************************************/
 
 $(document).ready(function() {
 
-	console.log('Holy Mountain Analytics Initiated');
+	//console.log('Holy Mountain Analytics Initiated');
 
 	/***** PLUGIN FUNCTION - targets specific elements *****/
-		$.fn.autotagger = function(category, action, value) {
+		$.fn.autotagger = function(category, action, value, label_format) {
 		    
 			$(this).each(function(){
 				if($(this).attr('data-event')){
@@ -126,6 +147,7 @@ $(document).ready(function() {
 
 					//console.log notification
 						console.log('GA Auto-Tagger - element tagged - category: '+category);
+					
 					//label grabber
 						if($(this).children().is('img')){
 							var label = $(this).attr('alt');
@@ -134,14 +156,22 @@ $(document).ready(function() {
 						} else {
 							var label = $(this).text();
 						}
-					//label formatter
-						label = label.replace(/\ /g, "-");
 					
-					$(this).attr('data-event', 'GAEvent');
-					$(this).attr('data-category', category);
-					$(this).attr('data-label', label);
-					$(this).attr('data-action', action);
-					$(this).attr('data-value', value);
+					//label formatter
+						if(label_format != ""){
+							label = label.replace(label_format);
+						} else{
+							//Normal formatter - replaces spaces with dashes
+							label = label.replace(/\ /g, "-");
+						}
+						
+					
+					//set attributes
+						$(this).attr('data-event', 'GAEvent');
+						$(this).attr('data-category', category);
+						$(this).attr('data-label', label);
+						$(this).attr('data-action', action);
+						$(this).attr('data-value', value);
 
 				}
 			});
@@ -152,3 +182,49 @@ $(document).ready(function() {
 
 
 
+
+/********************************************
+
+GA EVENTS - FORM SUBMISSIONS
+
+	- This block of code is for firing an event after a form 
+	has been successfully submitted.  There should be an 
+	event tag on the submit button, but this is specifically 
+	for after the form submits and the page refreshes.
+
+
+	urlParams.has('parameter'); 	// check if parameter exists
+	urlParams.get('parameter'); 	// get value of parameter
+	
+********************************************/
+$(document).ready(function() {
+	//Check if GAEvent param exists
+	if (urlParams.has('GAEvent')) {
+
+		//Set vars
+		var evCat = $(this).attr('data-category') 	? $(this).attr('data-category') : '';
+		var evAct = $(this).attr('data-action') 		? $(this).attr('data-action') : '';
+		var evLab = $(this).attr('data-label') 		? $(this).attr('data-label') : '';
+		var evVal = $(this).attr('data-value') 		? $(this).attr('data-value') : '';
+
+		try {
+			//Fire event
+			window.dataLayer = window.dataLayer || [];
+			dataLayer.push({
+				'event': 			'GAEvent',
+				'eventCategory': 	evCat,
+				'eventAction': 		evAct,
+				'eventLabel': 		evLab,
+				'eventValue': 		evVal,
+			});
+
+			console.log("GA Event fired - Event Category: ["+evCat+"], Event Label: ["+evLab+"], Event Action: ["+evAct+"]");
+
+		} catch (e) {
+			console.log("GA Event Error");
+		}
+
+	}else{
+		//do nothing
+	}
+});
--- script.js
+++ script.js
@@ -4,21 +4,30 @@ $(document).ready(function() {
 
 GA AUTO-TAGGER
 
+NOTES:
+	Make sure you have all autotagger instances run before you 
+	initialize the .eventfire_ready function to ensure all tags are caught.
+
+	Use label_format var to set custom label formatting.
+	Refer to main script for formatting example.
+
 ********************************************/
 	
 	/***** NAV *****/
-		var category	= "Nav";
-		var action		= "Click";
-		var value		= "NULL";
+		var category		= "Nav";
+		var action			= "Click";
+		var value			= "";
+		var label_format	="";
 
-		$('nav a').autotagger(category, action, value);
+		$('nav a').autotagger(category, action, value, label_format);
 
 	/***** FOOTER *****/
-		var category	= "Footer";
-		var action		= "Click";
-		var value		= "NULL";
+		var category		= "Footer";
+		var action			= "Click";
+		var value			= "";
+		var label_format	= "";
 
-		$('footer a').autotagger(category, action, value);
+		$('footer a').autotagger(category, action, value, label_format);
 	
 	/***** INITIALIZE *****/
 		$('html').eventfire_ready();
